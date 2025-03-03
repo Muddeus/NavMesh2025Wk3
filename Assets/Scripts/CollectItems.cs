fda5696058ed745f406966b305e2f32c94b1c02d
Muddeus <kainedavidobrien@gmail.com>
2025-03-03 14:40:34 +1100

    Added all 3 agents and new maze map

--- Assets/Scripts/CollectItems.cs
+++ Assets/Scripts/CollectItems.cs
@@ -1,23 +1,56 @@
+using System.Linq;
 using UnityEngine;
+using UnityEngine.AI;
+using System.Collections.Generic;
 
 public class CollectItems : MonoBehaviour
 {
     public SlidingDoor door;
     public int collectedItems = 0;
-    public int requiredItems = 3;
+
+    private NavMeshAgent agent;
+
+    List<GameObject> collectables;
+    private GameObject currentCollectable = null;
+
+
+    public void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        collectables = GameObject.FindGameObjectsWithTag("Collectable").ToList();
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        if (collectedItems >= requiredItems)
+        if(currentCollectable == null && collectables.Count > 0)
+        {
+            int randomIndex = Random.Range(0, collectables.Count);
+            currentCollectable = collectables[randomIndex];
+            collectables.RemoveAt(randomIndex);
+        }
+
+
+        if (currentCollectable != null)
+        {
+            agent.destination = currentCollectable.transform.position;
+        }
+
+        if(collectables.Count == 0)
         {
             door.NonagonInfinity();
         }
+
+
     }
     private void OnTriggerEnter(Collider other)
     {
+
         if (other.gameObject.tag == "Collectable")
         {
+            Debug.Log("bebebfe");
             collectedItems++;
             Destroy(other.gameObject);
         }
